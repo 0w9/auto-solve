db4455dac718c723a4998ab1b7fd8bb2bf6f3d35
Lennard Dorst <lennardsolana@gmail.com>
2023-05-23 10:14:00 +0200

    Parsing an image and extract the text from it

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from langchain.document_loaders.image import UnstructuredImageLoader
+from langchain.chat_models import ChatOpenAI
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import ( ChatPromptTemplate, SystemMessagePromptTemplate, AIMessagePromptTemplate, HumanMessagePromptTemplate )
+from langchain.schema import ( AIMessage, HumanMessage, SystemMessage )
+import os
+
+os.environ["OPENAI_API_KEY"] = "<enter your key here, or set it as an environment variable>"
+
+chat = ChatOpenAI(temperature=0)
 
 loader = UnstructuredImageLoader("test_fr.jpeg")
 data = loader.load()
-print(data[0])
+
+raw_data = data[0].page_content
+
+data = chat([HumanMessage(content=f'Format this text: {raw_data}')])
+
+
+
+with open("output.txt", "w") as f:
+    f.write(data[0].content)
